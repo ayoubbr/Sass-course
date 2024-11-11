ea7b1a4113ba01431d51b68c25f99cf6a47b4875
ayoubbr <ayoub1benmansour@gmail.com>
2024-11-11 11:31:02 +0100

    using multiple scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
+    const { src, dest, watch, series } = require('gulp')
+    const sass = require('gulp-sass')(require('sass'))
 
 
-function buildStyles() {
-    return src('index.scss')
-        .pipe(sass())
-        .pipe(dest('css'))
-}
+    function buildStyles() {
+        return src('*.scss')
+            .pipe(sass())
+            .pipe(dest('css'))
+    }
 
-function watchTask() {
-    watch(['index.scss'], buildStyles)
-}
+    function watchTask() {
+        watch(['*.scss'], buildStyles)
+    }
 
-exports.default = series(buildStyles, watchTask)
+    exports.default = series(buildStyles, watchTask)
